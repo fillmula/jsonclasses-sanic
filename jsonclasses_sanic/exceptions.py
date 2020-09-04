987b8e48ceccc2b72c80c94e36169a669a62d88e
Victor Zhang <yeannylam@gmail.com>
2020-09-04 08:59:49 +0800

    traceback

--- jsonclasses_sanic/exceptions.py
+++ jsonclasses_sanic/exceptions.py
@@ -2,6 +2,7 @@ from sanic.request import Request
 from sanic.exceptions import SanicException
 from jsonclasses.exceptions import ObjectNotFoundException, ValidationException
 from sanic.response import json
+from traceback import extract_tb
 
 class UnsupportedMediaTypeException(Exception):
 
@@ -24,6 +25,7 @@ def exception_handler(request: Request, exception: Exception):
   return json({
     'error': {
       'type': exception.__class__.__name__,
-      'message': exception.message if hasattr(exception, 'message') else str(exception)
+      'message': exception.message if hasattr(exception, 'message') else str(exception),
+      'traceback': [ f'file {f.filename} line {f.lineno} in {f.name}' for f in extract_tb(exception.__traceback__) ]
     }
   }, status=code)
