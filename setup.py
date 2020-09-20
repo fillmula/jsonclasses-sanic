fe852daff5e9eac323ba262027997ac37f7706b3
Victor Zhang <yeannylam@gmail.com>
2020-09-20 18:41:55 +0800

    bump major version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='jsonclasses-sanic',
-      version='0.6.8',
+      version='0.7.0',
       description='JSON Classes integration with sanic.',
       long_description=README,
       long_description_content_type="text/markdown",
