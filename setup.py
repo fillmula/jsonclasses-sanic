43a269230063a60b9467255435d748681a8d0db5
Victor Zhang <yeannylam@gmail.com>
2021-02-05 17:06:59 +0800

    fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='jsonclasses-sanic',
-      version='1.0.3',
+      version='1.0.4',
       description='JSON Classes integration with sanic.',
       long_description=README,
       long_description_content_type="text/markdown",
